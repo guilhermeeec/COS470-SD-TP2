16584daa6b35f1ad328a59eeed782d55b4493ca9
guilhermeeec <guilhermepic2016@gmail.com>
2023-05-06 13:00:11 -0300

    Funcionando, mas faltam testes intensivos

--- Makefile
+++ Makefile
@@ -2,14 +2,18 @@ C = g++
 CFLAGS = -Wall
 
 TIMERS_SRCS = timer.cpp example_timer.cpp
+PROD_CONS_SRCS = produtor-consumidor.cpp
 
-all: timer
+all: timer prod_cons
 
 timer: $(TIMERS_SRCS:.c=.o)
 	$(CC) -o test_timer example_timer.o timer.o
+
+prod_cons: $(PROD_CONS_SRCS:.c=.o)
+	$(CC) -o prod_cons prod_cons.o
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -f test_timer $(wildcard *.o)
+	rm -f test_timer prod_cons $(wildcard *.o)
--- produtor-consumidor.cpp
+++ produtor-consumidor.cpp
@@ -24,36 +24,36 @@
 // -------------- Variáveis globais --------------
 
 sem_t mutex, empty, full;
-bool consumo_em_andamento = true;
 std::queue<uint32_t> buffer;
 uint32_t numero_produtos_consumidos = 0;
-uint32_t numero_max_produtos_consumidos = pow(10,5);
+uint32_t numero_produtos_produzidos = 0;
+uint32_t numero_max_produtos = pow(10,1);
 
 // -------------- Funções dos produtores e consumidores --------------
 
-uint32_t produz_inteiro() 
+uint32_t produz_inteiro(unsigned id) 
 {
-    srand((unsigned) time(NULL));
-    return (uint32_t)((rand() / RAND_MAX) * (pow(10,7)-1) + 1);
+    return (uint32_t)(rand() % (int)pow(10,7))+1;
 }
 
 void checar_primo(int numero) {
     if (numero == 1) {
-        std::cout << numero << "nao eh primo" << std::endl;
+        std::cout << numero << " nao eh primo" << std::endl << std::flush;
         return;
     }
     for (int idx = 2; idx <= numero/2; idx++)
         if (!(numero % idx)) {
-            std::cout << numero << "nao eh primo" << std::endl;
+            std::cout << numero << " nao eh primo" << std::endl << std::flush;
             return;
         }
-    std::cout << numero << "eh primo" << std::endl;
+    std::cout << numero << " eh primo" << std::endl << std::flush;
     return;
 }
 
 void colocar_no_buffer(uint32_t produto) 
 {
     buffer.push(produto);
+    numero_produtos_produzidos++;
 }
 
 uint32_t retirar_do_buffer() 
@@ -64,39 +64,39 @@ uint32_t retirar_do_buffer()
     return produto;
 }
 
-void tarefa_produtor() 
+void tarefa_produtor(unsigned id) 
 {
-    while(1) {        
-        uint32_t produto = produz_inteiro();
+    bool continua_producao = true;
+    srand((unsigned) time(NULL)+id);
+    while(continua_producao) {        
+        uint32_t produto = produz_inteiro(id);
         sem_wait(&empty);
         sem_wait(&mutex);
-        if(numero_produtos_consumidos == numero_max_produtos_consumidos) {
-            sem_post(&mutex);
-            sem_post(&full);
-            std::terminate();
+        if(numero_produtos_produzidos == numero_max_produtos)
+            continua_producao = false;
+        else {
+            colocar_no_buffer(produto);
+            std::cout << "[PROD" << id << "] colocou " << produto << " no buffer" << std::endl << std::flush;
         }
-        colocar_no_buffer(produto);
         sem_post(&mutex);
         sem_post(&full);
     }
 }
 
-void tarefa_consumidor() 
+void tarefa_consumidor(unsigned id) 
 {
-    while(1) {
+    bool continua_consumo = true;
+    uint32_t produto = 0;
+    while(continua_consumo) {
         sem_wait(&full);
         sem_wait(&mutex);
-        if(numero_produtos_consumidos == numero_max_produtos_consumidos) {
-            sem_post(&mutex);
-            sem_post(&empty);
-            std::terminate();
-        }
-        uint32_t produto = retirar_do_buffer();
+        if(numero_produtos_consumidos == numero_max_produtos)
+            continua_consumo = false;
+        else
+            produto = retirar_do_buffer();
         sem_post(&mutex);
         sem_post(&empty);
-        checar_primo(produto);
-        if(numero_produtos_consumidos == numero_max_produtos_consumidos)
-            std::terminate();
+        if(continua_consumo) checar_primo(produto);
     }
 }
 
@@ -104,16 +104,16 @@ void tarefa_consumidor()
 
 int main() 
 {
-    // Inicializa semáforos
+    std::cout << "[MAIN] Inicializando semaforos" << std::endl << std::flush;
     sem_init(&mutex, 0, 1);
     sem_init(&empty, 0, N);
     sem_init(&full, 0, 0);
 
-    // Cria threads
+    std::cout << "[MAIN] Criando threads" << std::endl << std::flush;
     std::vector<std::thread> vetor_consumidores;
     std::vector<std::thread> vetor_produtores;
     srand((unsigned) time(NULL));
-    while((vetor_produtores.size() < Np) && (vetor_consumidores.size() < Nc)) 
+    while((vetor_produtores.size() < Np) || (vetor_consumidores.size() < Nc)) 
     {
         // Decide aleatoriamente se vai criar produor (0) ou consumidor (1)
         int zero_ou_um = rand() % 2;
@@ -121,16 +121,18 @@ int main()
         if(vetor_consumidores.size() == Nc) zero_ou_um = 0;
 
         if(zero_ou_um == 0) 
-            vetor_produtores.push_back(std::thread(tarefa_produtor));
+            vetor_produtores.push_back(std::thread(tarefa_produtor,vetor_produtores.size()));
         else
-            vetor_consumidores.push_back(std::thread(tarefa_consumidor));
+            vetor_consumidores.push_back(std::thread(tarefa_consumidor,vetor_consumidores.size()));
     }
 
     // Espera threads terminarem
+    std::cout << "[MAIN] Esperando " << vetor_produtores.size() << " produtores e " << vetor_consumidores.size() << " consumidores" << std::endl << std::flush;
     for(std::thread &thread_produtor : vetor_produtores) 
         thread_produtor.join();
     for(std::thread &thread_consumidor : vetor_consumidores) 
         thread_consumidor.join();
 
+    std::cout << "[MAIN] Terminou" << std::endl << std::flush;
     return 0;
 }
