742186bdf0dff55d429473f0b551e71d86eb3ec8
guilhermeeec <guilhermepic2016@gmail.com>
2023-05-07 13:02:26 -0300

    Testado para casos mais complexos

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
-C = g++
+CC = g++
 CFLAGS = -Wall
+
+PROD_CONS_N=10
+PROD_CONS_Np=4
+PROD_CONS_Nc=4
 
 TIMERS_SRCS = timer.cpp example_timer.cpp
 PROD_CONS_SRCS = produtor-consumidor.cpp
 
 all: timer prod_cons
 
-timer: $(TIMERS_SRCS:.c=.o)
+timer: $(TIMERS_SRCS:.cpp=.o)
 	$(CC) -o test_timer example_timer.o timer.o
 
-prod_cons: $(PROD_CONS_SRCS:.c=.o)
-	$(CC) -o prod_cons prod_cons.o
+prod_cons: $(PROD_CONS_SRCS:.cpp=.o)
+	$(CC) -pthread -o prod_cons produtor-consumidor.o
 
-%.o: %.c
+produtor-consumidor.o: produtor-consumidor.cpp
+	$(CC) ${CFLAGS} -DN=${PROD_CONS_N} -DNc=${PROD_CONS_Nc} -DNp=${PROD_CONS_Np} -c produtor-consumidor.cpp -o produtor-consumidor.o
+
+%.o: %.cpp
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
--- produtor-consumidor.cpp
+++ produtor-consumidor.cpp
@@ -13,13 +13,19 @@
 // -------------- Macros --------------
 
 // Tamanho do vetor de inteiros
+#ifndef N
 #define N   10
+#endif
 
 // Número de threads de produtores
+#ifndef Np
 #define Np  4
+#endif
 
 // Número de threads de consumidores
+#ifndef Nc
 #define Nc  4
+#endif
 
 // -------------- Variáveis globais --------------
 
@@ -27,7 +33,7 @@ sem_t mutex, empty, full;
 std::queue<uint32_t> buffer;
 uint32_t numero_produtos_consumidos = 0;
 uint32_t numero_produtos_produzidos = 0;
-uint32_t numero_max_produtos = pow(10,1);
+uint32_t numero_max_produtos = pow(10,5);
 
 // -------------- Funções dos produtores e consumidores --------------
 
@@ -36,17 +42,17 @@ uint32_t produz_inteiro(unsigned id)
     return (uint32_t)(rand() % (int)pow(10,7))+1;
 }
 
-void checar_primo(int numero) {
+void checar_primo(int numero, unsigned id) {
     if (numero == 1) {
-        std::cout << numero << " nao eh primo" << std::endl << std::flush;
+        std::cout << "[CONS" << id << "] (produto " << numero_produtos_consumidos << ") " << numero << " nao eh primo" << std::endl << std::flush;
         return;
     }
     for (int idx = 2; idx <= numero/2; idx++)
         if (!(numero % idx)) {
-            std::cout << numero << " nao eh primo" << std::endl << std::flush;
+            std::cout << "[CONS" << id << "] " << numero << " nao eh primo" << std::endl << std::flush;
             return;
         }
-    std::cout << numero << " eh primo" << std::endl << std::flush;
+    std::cout << "[CONS" << id << "] " << numero << " eh primo" << std::endl << std::flush;
     return;
 }
 
@@ -76,7 +82,7 @@ void tarefa_produtor(unsigned id)
             continua_producao = false;
         else {
             colocar_no_buffer(produto);
-            std::cout << "[PROD" << id << "] colocou " << produto << " no buffer" << std::endl << std::flush;
+            //std::cout << "[PROD" << id << "] colocou " << produto << " no buffer" << std::endl << std::flush;
         }
         sem_post(&mutex);
         sem_post(&full);
@@ -96,7 +102,7 @@ void tarefa_consumidor(unsigned id)
             produto = retirar_do_buffer();
         sem_post(&mutex);
         sem_post(&empty);
-        if(continua_consumo) checar_primo(produto);
+        if(continua_consumo) checar_primo(produto, id);
     }
 }
 
@@ -104,6 +110,7 @@ void tarefa_consumidor(unsigned id)
 
 int main() 
 {
+    std::cout << "[MAIN] Tamanho do buffer: " << N << std::endl << std::flush;
     std::cout << "[MAIN] Inicializando semaforos" << std::endl << std::flush;
     sem_init(&mutex, 0, 1);
     sem_init(&empty, 0, N);
