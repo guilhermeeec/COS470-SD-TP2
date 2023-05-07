186b758fe3b35a9535abccd5afc5596f30347107
guilhermedionisio <gsantanadionisio@gmail.com>
2023-05-07 12:47:00 -0300

    Somador no Makefile e função de timer adicionada ao somador

--- Makefile
+++ Makefile
@@ -2,14 +2,18 @@ CC = g++
 CFLAGS = -Wall
 
 TIMERS_SRCS = timer.cpp example_timer.cpp
+SOMADOR_SRCS = somador.cpp spinlock.cpp
 
-all: timer
+all: timer somador
 
 timer: $(TIMERS_SRCS:.cpp=.o)
 	$(CC) -o test_timer example_timer.o timer.o
+
+somador: $(SOMADOR_SRCS:.cpp=.o)
+	$(CC) -std=c++11 -pthread -o somador somador.o spinlock.o
 
 %.o: %.cpp
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -f test_timer $(wildcard *.o)
+	rm -f test_timer somador $(wildcard *.o)
--- somador.cpp
+++ somador.cpp
@@ -3,76 +3,99 @@
 #include <ctime>
 #include <iostream>
 #include <thread>
+#include <chrono>
 #include "spinlock.h"
 
 double acumulador_compartilhado = 0;
 double acumulador_checagem = 0;
-SpinLock somador_lock;
+float tempo_total = 0;
 
-std::vector<std::int8_t> gerar_numeros(double n) {
-    std::mt19937 gerador(time(NULL)); // Gerador Mersenne Twister
+SpinLock somador_lock;
+SpinLock tempo_lock;
+
+std::vector<std::int8_t> gerar_numeros(double n)
+{
+    std::mt19937 gerador(time(NULL));                           // Gerador Mersenne Twister
     std::uniform_int_distribution<std::int8_t> dist(-100, 100); // Distribuição entre (-100, 100)
     std::vector<std::int8_t> numeros;
 
     // Gerar n números aleatórios e adicionar ao vetor de números
-    for(double idx = 0; idx < n; idx++) {
+    for (double idx = 0; idx < n; idx++)
+    {
         numeros.push_back(dist(gerador));
     }
 
     return numeros;
 }
 
-void somar_parcela(std::vector<int8_t> parcela) {
+void somar_parcela(std::vector<int8_t> parcela)
+{
     double temp = 0;
-    
+    auto inicio = std::chrono::high_resolution_clock::now();
+
     // Faz a soma da parcela correspondente àquela thread
-    for (double idx = 0; idx < parcela.size(); idx++) {
-        temp += parcela[idx];
+    for (auto& num: parcela)
+    {
+        temp += num;
     }
     // Adquire controle da região crítica, adiciona ao acumulador e libera a região crítica
     somador_lock.acquire();
     acumulador_compartilhado += temp;
+    auto fim = std::chrono::high_resolution_clock::now();
     somador_lock.release();
 
+    tempo_lock.acquire();
+    tempo_total += std::chrono::duration<double, std::milli>(fim - inicio).count();
+    tempo_lock.release();
 }
 
-void somar_checagem (std::vector<int8_t> total) {
+void somar_checagem(std::vector<int8_t> total)
+{
     // Soma todos os valores para a checagem
-    for (double idx = 0; idx < total.size(); idx++) {
-        acumulador_checagem += total[idx];
+    for (auto& num: total)
+    {
+        acumulador_checagem += num;
     }
 }
 
-int main() {
+int main()
+{
     std::vector<double> n_numeros = {10000000, 100000000, 1000000000};
     std::vector<int> k_threads = {1, 2, 4, 8, 16, 32, 64, 128, 256};
 
-    for (int n = 0; n < n_numeros.size(); n++) {
-        std::vector<std::int8_t> numeros = gerar_numeros(n_numeros[n]);
-        std::cout << "Números Gerados: " << n_numeros[n] << std::endl;
-        for (int k = 0; k < k_threads.size(); k++) {
+    for (auto& n: n_numeros) {
+        std::vector<std::int8_t> numeros = gerar_numeros(n);
+        std::cout << "Números Gerados: " << n << std::endl;
+        for (auto& k: k_threads) {
             // Gerar o vetor de threads para as k threads
-            std::vector<std::thread> threads(k_threads[k] + 1);
-            // Apontar para cada thread a parcela específica a ser somada
-            for (int idx = 0; idx < k_threads[k]; idx++) {
-                threads[idx] = std::thread(somar_parcela,
-                                           std::vector<int8_t>(numeros.begin() + idx*(n_numeros[n]/k_threads[k]),
-                                                            numeros.begin() + (idx+1)*(n_numeros[n]/k_threads[k])));
-            }
-            // Criar a thread para checagem
-            threads[threads.size() - 1] = std::thread(somar_checagem, numeros);
-        
-            // Esperar que todas as threads terminem
-            for (int idx = 0; idx < k_threads[k] + 1; idx++) {
-                threads[idx].join();
-            }
-            std::cout << k_threads[k] << " Threads:" << std::endl;
-            std::cout << "Soma Compartilhada: " << acumulador_compartilhado << std::endl;
-            std::cout << "Soma para Checagem: " << acumulador_checagem << std::endl;
+            std::vector<std::thread> threads(k + 1);
+            for (int i = 0; i < 10; i++) {
+                // Apontar para cada thread a parcela específica a ser somada
+                for (int idx = 0; idx < k; idx++) {
+                    threads[idx] = std::thread(somar_parcela,
+                                               std::vector<int8_t>(numeros.begin() + idx * (n / k),
+                                                                   numeros.begin() + (idx + 1) * (n / k)));
+                }
+                // Criar a thread para checagem
+                threads[threads.size() - 1] = std::thread(somar_checagem, numeros);
 
-            acumulador_compartilhado = 0;
-            acumulador_checagem = 0;
-        }  
+                // Esperar que todas as threads terminem
+                for (auto& thread: threads) {
+                    thread.join();
+                }
+                
+                std::cout << k << " Threads:" << std::endl;
+                std::cout << "Soma Compartilhada: " << acumulador_compartilhado << std::endl;
+                std::cout << "Soma para Checagem: " << acumulador_checagem << std::endl;
+
+                acumulador_compartilhado = 0;
+                acumulador_checagem = 0;
+            }
+            float tempo_total_medio = tempo_total/10;
+            tempo_total = 0;
+            std::cout << std::endl;
+            std::cout << "Tempo Médio:" << tempo_total_medio << "ms" << std::endl;
+            std::cout << std::endl;
+        }
     }
-    
 }
