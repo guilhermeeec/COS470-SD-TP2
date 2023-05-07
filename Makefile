f9a39f0e334693a867a81e59333fc4c13fbefacd
guilhermeeec <guilhermepic2016@gmail.com>
2023-05-07 16:27:30 -0300

    Avalia ocupacao do buffer (bug no encerramento)

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ CFLAGS = -Wall
 PROD_CONS_N=10
 PROD_CONS_Np=4
 PROD_CONS_Nc=4
+PROD_CONS_FILENAME=ocupacao_buffer.txt
 
 TIMERS_SRCS = timer.cpp example_timer.cpp
 PROD_CONS_SRCS = produtor-consumidor.cpp
@@ -17,7 +18,7 @@ prod_cons: $(PROD_CONS_SRCS:.cpp=.o)
 	$(CC) -pthread -o prod_cons produtor-consumidor.o
 
 produtor-consumidor.o: produtor-consumidor.cpp
-	$(CC) ${CFLAGS} -DN=${PROD_CONS_N} -DNc=${PROD_CONS_Nc} -DNp=${PROD_CONS_Np} -c produtor-consumidor.cpp -o produtor-consumidor.o
+	$(CC) ${CFLAGS} -DFILENAME="\"${PROD_CONS_FILENAME}\"" -DN=${PROD_CONS_N} -DNc=${PROD_CONS_Nc} -DNp=${PROD_CONS_Np} -c produtor-consumidor.cpp -o produtor-consumidor.o
 
 %.o: %.cpp
 	$(CC) $(CFLAGS) -c $< -o $@
--- produtor-consumidor.cpp
+++ produtor-consumidor.cpp
@@ -8,6 +8,7 @@
 #include <semaphore.h>
 #include <queue>
 #include <vector>
+#include <fstream>
 //#include "utils.h" // Trabalho anterior
 
 // -------------- Macros --------------
@@ -27,13 +28,19 @@
 #define Nc  4
 #endif
 
+// Nome do arquivo com ocupação do buffer
+#ifndef FILENAME
+#define FILENAME "ocupcao_buffer.txt"
+#endif
+
 // -------------- Variáveis globais --------------
 
 sem_t mutex, empty, full;
 std::queue<uint32_t> buffer;
 uint32_t numero_produtos_consumidos = 0;
 uint32_t numero_produtos_produzidos = 0;
-uint32_t numero_max_produtos = pow(10,5);
+uint32_t numero_max_produtos = pow(10,1);
+std::vector<uint32_t> ocupacao_buffer;
 
 // -------------- Funções dos produtores e consumidores --------------
 
@@ -60,6 +67,7 @@ void colocar_no_buffer(uint32_t produto)
 {
     buffer.push(produto);
     numero_produtos_produzidos++;
+    ocupacao_buffer.push_back(numero_produtos_produzidos-numero_produtos_consumidos);
 }
 
 uint32_t retirar_do_buffer() 
@@ -67,6 +75,7 @@ uint32_t retirar_do_buffer()
     uint32_t produto = buffer.front();
     buffer.pop();
     numero_produtos_consumidos++;
+    ocupacao_buffer.push_back(numero_produtos_produzidos-numero_produtos_consumidos);
     return produto;
 }
 
@@ -74,19 +83,20 @@ void tarefa_produtor(unsigned id)
 {
     bool continua_producao = true;
     srand((unsigned) time(NULL)+id);
-    while(continua_producao) {        
+    while(continua_producao) { 
         uint32_t produto = produz_inteiro(id);
         sem_wait(&empty);
         sem_wait(&mutex);
-        if(numero_produtos_produzidos == numero_max_produtos)
+        if(numero_produtos_consumidos == numero_max_produtos)
             continua_producao = false;
         else {
             colocar_no_buffer(produto);
-            //std::cout << "[PROD" << id << "] colocou " << produto << " no buffer" << std::endl << std::flush;
+            std::cout << "[PROD" << id << "] colocou " << produto << " (produto " << numero_produtos_produzidos << ") no buffer" << std::endl << std::flush;
         }
         sem_post(&mutex);
         sem_post(&full);
     }
+    std::cout << "[PROD" << id << "] fim" << std::endl << std::flush;
 }
 
 void tarefa_consumidor(unsigned id) 
@@ -104,13 +114,18 @@ void tarefa_consumidor(unsigned id)
         sem_post(&empty);
         if(continua_consumo) checar_primo(produto, id);
     }
+    std::cout << "[CONS" << id << "] fim" << std::endl << std::flush;
 }
 
 // -------------- Funções básicas --------------
 
 int main(int argc, char** argv) 
 {
+    std::cout << "[MAIN] Arquivo de ocupacao do buffer: " << FILENAME << std::endl << std::flush;
+
     std::cout << "[MAIN] Tamanho do buffer: " << N << std::endl << std::flush;
+    ocupacao_buffer.push_back(0);
+
     std::cout << "[MAIN] Inicializando semaforos" << std::endl << std::flush;
     sem_init(&mutex, 0, 1);
     sem_init(&empty, 0, N);
@@ -127,10 +142,14 @@ int main(int argc, char** argv)
         if(vetor_produtores.size() == Np) zero_ou_um = 1;
         if(vetor_consumidores.size() == Nc) zero_ou_um = 0;
 
-        if(zero_ou_um == 0) 
-            vetor_produtores.push_back(std::thread(tarefa_produtor,vetor_produtores.size()));
-        else
-            vetor_consumidores.push_back(std::thread(tarefa_consumidor,vetor_consumidores.size()));
+        if(zero_ou_um == 0) {
+            vetor_produtores.push_back(std::thread(tarefa_produtor,vetor_produtores.size()+1));
+            std::cout << "[MAIN] Criei [PROD" << vetor_produtores.size() << "]" << std::endl << std::flush;
+        }
+        else {
+            vetor_consumidores.push_back(std::thread(tarefa_consumidor,vetor_consumidores.size()+1));
+            std::cout << "[MAIN] Criei [CONS" << vetor_consumidores.size() << "]" << std::endl << std::flush;
+        }
     }
 
     // Espera threads terminarem
@@ -139,6 +158,15 @@ int main(int argc, char** argv)
         thread_produtor.join();
     for(std::thread &thread_consumidor : vetor_consumidores) 
         thread_consumidor.join();
+
+
+    std::cout << "[MAIN] Salvando ocupacao do buffer em disco" << std::endl << std::flush;
+    std::ofstream arquivo(FILENAME);
+    if (!arquivo.is_open())
+        std::cout << "[MAIN] Nao foi possivel abrir arquivo" << std::endl;
+    for(auto ocupacao : ocupacao_buffer)
+        arquivo << ocupacao << std::endl;
+    arquivo.close();
 
     std::cout << "[MAIN] Terminou" << std::endl << std::flush;
     return 0;
