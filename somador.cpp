9fbd763db90cb0a4c3d365f3eb193ed9251f84f6
guilhermedionisio <gsantanadionisio@gmail.com>
2023-05-08 21:20:21 -0300

    Implementação de SpinLock incluindo guardar os resultados em um arquivo

--- somador.cpp
+++ somador.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
 #include "spinlock.h"
 
 double acumulador_compartilhado = 0;
@@ -63,12 +65,15 @@ int main()
     std::vector<double> n_numeros = {10000000, 100000000, 1000000000};
     std::vector<int> k_threads = {1, 2, 4, 8, 16, 32, 64, 128, 256};
 
+    std::ofstream arquivo_saida("medias.txt");
+
     for (auto& n: n_numeros) {
         std::vector<std::int8_t> numeros = gerar_numeros(n);
         std::cout << "Números Gerados: " << n << std::endl;
         for (auto& k: k_threads) {
             // Gerar o vetor de threads para as k threads
             std::vector<std::thread> threads(k + 1);
+            std::cout << k << " Threads:" << std::endl;
             for (int i = 0; i < 10; i++) {
                 // Apontar para cada thread a parcela específica a ser somada
                 for (int idx = 0; idx < k; idx++) {
@@ -84,18 +89,28 @@ int main()
                     thread.join();
                 }
                 
-                std::cout << k << " Threads:" << std::endl;
-                std::cout << "Soma Compartilhada: " << acumulador_compartilhado << std::endl;
-                std::cout << "Soma para Checagem: " << acumulador_checagem << std::endl;
+                if (acumulador_compartilhado != acumulador_checagem) {
+                    std::cout << "Valores somados inconsistentes com a checagem." << std::endl;
+                    return 1;
+                }
+                //std::cout << "Soma Compartilhada: " << acumulador_compartilhado << std::endl;
+                //std::cout << "Soma para Checagem: " << acumulador_checagem << std::endl;
 
                 acumulador_compartilhado = 0;
                 acumulador_checagem = 0;
             }
-            float tempo_total_medio = tempo_total/10;
+            int tempo_total_medio = round(tempo_total/10);
+
             tempo_total = 0;
+            if (arquivo_saida.is_open()) {
+                arquivo_saida << tempo_total_medio << " ";
+            }
             std::cout << std::endl;
             std::cout << "Tempo Médio:" << tempo_total_medio << "ms" << std::endl;
             std::cout << std::endl;
         }
+        arquivo_saida << "\n";
     }
+    arquivo_saida.close();
+    return 0;
 }
